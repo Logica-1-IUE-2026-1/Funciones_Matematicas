012a6bf4b791db5097d792c45fa6be035b2afecd
josemon745 <josemanuelmontoya725@gmail.com>
2026-04-20 20:05:34 -0500

    Create circulo.py

---
+++ circulo.py
@@ -0,0 +1,18 @@
+import math 
+def area_circulo(radio):
+   """ esta funcion calcula el area de un circulo dando su radio
+   parametros = el radio del círculo
+   retorna = el area del circulo con el radio dado"""
+   z = math.pi * radio ** 2
+   return z
+radiox = int(input("Ingrese el radio del círculo: "))
+print(f"El área del círculo es: {area_circulo(radiox)}")
+
+def perimetro_circulo(radio):
+    """esta funcion calcula el perímetro de un circulo dado su radio
+    parametros = el radio del círculo
+    retorna = el perímetro del círculo con el radio dado"""
+    z = 2 * math.pi * radio
+    return a
+print(f"El perímetro del círculo es: {perimetro_circulo(radiox)}")
+
